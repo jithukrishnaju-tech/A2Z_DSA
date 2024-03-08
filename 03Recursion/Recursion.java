40109e126bd98f5330cf96261054dc8e2c2eab93
Jithu <jithukrishnaju@gmail.com>
2024-03-08 23:19:39 +0530

    New Recursion

--- 03Recursion/Recursion.java
+++ 03Recursion/Recursion.java
@@ -1,7 +1,47 @@
 public class Recursion {
 
   public static void main(String[] args) {
-    System.out.println(factorial(4));
+    System.out.println(palindrome("madam".toCharArray(), 0, 5));
+    System.out.println(fibonacci(4));
+  }
+
+  static int fibonacci(int n) {
+    if (n <= 1) {
+      return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  static boolean palindrome(char[] str, int i, int n) {
+    if (str[i] != str[n - i - 1]) {
+      return false;
+    }
+    if (i >= n / 2) {
+      return true;
+    }
+    return palindrome(str, i + 1, n);
+  }
+
+  static char[] reverse1(char[] str, int i, int n) {
+    if (i >= n / 2) {
+      return str;
+    }
+    swap(i, n, str);
+    return reverse1(str, i + 1, n);
+  }
+
+  static char[] reverse(char[] str, int i, int n) {
+    if (i <= n) {
+      return str;
+    }
+    swap(i, n, str);
+    return reverse(str, i + 1, n);
+  }
+
+  static void swap(int l, int h, char[] str) {
+    char temp = str[l];
+    str[l] = str[h];
+    str[h] = temp;
   }
 
   static int factorial(int n) {
