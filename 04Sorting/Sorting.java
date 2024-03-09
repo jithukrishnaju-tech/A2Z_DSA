53854ac516aecb5773cc33d2526dd7cdba5750da
Jithu <jithukrishnaju@gmail.com>
2024-03-09 22:39:15 +0530

    Basic Sortin Algorithms

--- 04Sorting/Sorting.java
+++ 04Sorting/Sorting.java
@@ -3,11 +3,34 @@ import java.util.Arrays;
 public class Sorting {
   public static void main(String[] args) {
     int[] arr = { 1, 3, 5, 4, 2 };
-    bubbleSorting(arr, arr.length);
+    selectionSort(arr);
     System.out.println(Arrays.toString(arr));
   }
 
-  static void bubbleSorting(int[] arr, int len) {
+  static void selectionSort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      int j = i;
+      while (j > 0 && arr[j] < arr[j - 1]) {
+        swap(j - 1, j, arr);
+        j--;
+      }
+    }
+  }
+
+  static void insertionSort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      int min_index = i;
+
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[min_index]) {
+          swap(j, min_index, arr);
+        }
+      }
+
+    }
+  }
+
+  static void bubbleSorting(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
       for (int j = 1; j < arr.length - i; j++) {
         if (arr[j] < arr[j - 1]) {
