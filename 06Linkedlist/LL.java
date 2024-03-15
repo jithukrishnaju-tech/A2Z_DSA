389519115f96c5bca89e1c94435c24469c3dfaa4
Jithu <jithukrishnaju@gmail.com>
2024-03-15 21:24:49 +0530

    LL

--- 06Linkedlist/LL.java
+++ 06Linkedlist/LL.java
@@ -3,6 +3,40 @@ public class LL {
   public Node tail;
   public int length;
 
+  public void deleteatk(int k) {
+    if (head == null || head == tail) {
+      return;
+    }
+    Node temp = head;
+    Node pre = null;
+    int count = 0;
+    while (temp != null) {
+      count++;
+      if (count == k) {
+        pre.next = pre.next.next;
+        break;
+      }
+      pre = temp;
+      temp = temp.next;
+    }
+  }
+
+  public void deleteFirst() {
+    if (head == null) {
+      return;
+    }
+    head = head.next;
+  }
+
+  public void deleteLast() {
+    Node temp = head;
+    while (temp.next.next != null) {
+      temp = temp.next;
+    }
+    temp.next = null;
+    tail = temp;
+  }
+
   public void inserFirst(int value) {
     Node node = new Node(value);
     node.next = head;
@@ -10,6 +44,50 @@ public class LL {
     if (tail == null) {
       tail = head;
     }
+  }
+
+  public void arraytoLinkedlist(int[] arr) {
+    Node node = new Node(arr[0]);
+    head = node;
+    for (int i = 1; i < arr.length; i++) {
+      Node temp = new Node(arr[i]);
+      node.next = temp;
+      node = temp;
+    }
+  }
+
+  public int lengthofLL() {
+    Node temp = head;
+    while (temp != null) {
+      length++;
+      temp = temp.next;
+    }
+    return length;
+  }
+
+  public void insertKthindex(int k, int val) {
+    int count = 0;
+    Node temp = head;
+    while (temp != null) {
+      count++;
+      if (k == (count + 1)) {
+        Node inserNode = new Node(val);
+        inserNode.next = temp.next;
+        temp.next = inserNode;
+      }
+      temp = temp.next;
+    }
+  }
+
+  public boolean isThereinLL(int val) {
+    Node temp = head;
+    while (temp != null) {
+      if (temp.value == val) {
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
   }
 
   public void insertLast(int val) {
--- 06Linkedlist/Main.java
+++ 06Linkedlist/Main.java
@@ -1,14 +1,20 @@
 public class Main {
   public static void main(String[] args) {
     LL lin = new LL();
-    lin.inserFirst(1);
-    lin.inserFirst(2);
-    lin.inserFirst(3);
-    lin.inserFirst(4);
-    lin.inserFirst(5);
-    lin.inserFirst(6);
-    lin.insertLast(1);
+    // lin.inserFirst(1);
+    // lin.inserFirst(2);
+    // lin.inserFirst(3);
+    // lin.inserFirst(4);
+    // lin.inserFirst(5);
+    // lin.inserFirst(6);
+    // lin.insertLast(1);
+    // lin.display();
+    int[] arr = { 2, 4, 6, 8 };
+    lin.arraytoLinkedlist(arr);
     lin.display();
+    int len = lin.lengthofLL();
+    boolean isExists = lin.isThereinLL(1);
+    System.out.println(isExists);
 
   }
 }
